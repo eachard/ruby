e384781a285eb6606a57557e4c753f2d1d05c463
Etienne Achard <MacBookPro@MacBook-Pro-de-MacBookPro.local>
2014-01-07 11:20:14 +0100

    your name

--- Wagon-0/05. What's your name ?/introduce_yourself.rb
+++ Wagon-0/05. What's your name ?/introduce_yourself.rb
@@ -1,1 +1,17 @@
 # your code goes here, introduce yourself !
+def introduce_your_name
+	puts "What is your first name ?"
+	first_name = gets.chomp	
+
+	puts "What is your second name ?"
+	second_name = gets.chomp	
+
+	puts "What is your last name ?"
+	last_name = gets.chomp	
+
+	puts "Hello #{first_name} Welchome #{second_name} You're a bad person #{last_name} !!"
+end
+
+
+# Testing suite
+introduce_your_name
