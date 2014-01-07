bf25dd90df4372cbbd7c448b2c9006c3f22290be
Etienne Achard <MacBookPro@MacBook-Pro-de-MacBookPro.local>
2014-01-07 14:25:52 +0100

    stupid coaching

--- Wagon-0/06. Stupid coaching/stupid_coaching.rb
+++ Wagon-0/06. Stupid coaching/stupid_coaching.rb
@@ -1,5 +1,20 @@
 def stupid_coaching 
-  # your code goes here
+	
+	puts "what's up kid"
+	answer = gets.chomp
+
+	until answer == "Leave me alone"
+
+		if answer.end_with?("?")
+			puts "Silly question"
+		else
+			puts "I don't care son!"
+		end
+
+		answer = gets.chomp
+
+	end
+
 end
 
 # Calling the method
