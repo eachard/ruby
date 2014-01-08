76f23ce641b43fe475f4275f437c43f119bd6df1
Etienne Achard <MacBookPro@MacBook-Pro-de-MacBookPro.local>
2014-01-08 11:41:07 +0100

    simple looping

--- Wagon-1/01. Simple looping/simple_looping.rb
+++ Wagon-1/01. Simple looping/simple_looping.rb
@@ -2,6 +2,15 @@
 
 def sum(min,max)
   # your code here
+  answer = 0
+
+  # answer = 4
+  # min = 4
+  while min <= 100
+    answer = answer + min
+    min = min + 1
+  end
+  answer
 end
 
 # Testing your code
@@ -9,4 +18,3 @@ end
 min = 1
 max = 100
 sum = sum(1,100)
-puts sum == 5050 # => true
