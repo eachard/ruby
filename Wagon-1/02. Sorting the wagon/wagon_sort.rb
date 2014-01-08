caeebcd7efa451930b6011265742c965d1d2baac
Etienne Achard <MacBookPro@MacBook-Pro-de-MacBookPro.local>
2014-01-08 12:11:59 +0100

    wagon sort

--- Wagon-1/02. Sorting the wagon/wagon_sort.rb
+++ Wagon-1/02. Sorting the wagon/wagon_sort.rb
@@ -1,5 +1,18 @@
-def wagon_sort( students_array )
-  # your code to sort the array and output students
+def wagon_sort(students_array)
+ students_array.sort
 end
 
-# your code to interact with the user..
+students_array= []
+
+while true
+	puts "entrez votre nom"
+	student = gets.chomp.capitalize
+	break if student.empty?
+	students_array << student
+end
+
+students_count = students_array.length
+student_string = (students_count > 1) ? "students" : "student"
+
+puts "Congrats, you have #{students_count} #{student_string}"
+puts wagon_sort(students_array)
